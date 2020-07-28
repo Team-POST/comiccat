da6a124872233f73f00ac55e4eb89b1041ef8829
PVOBrien <PVOVideo@outlook.com>
2020-07-28 13:26:35 -0700

    correct seed data, initial correct edit.ejs page

--- server.js
+++ server.js
@@ -59,7 +59,26 @@ app.set('view engine', 'ejs');
 
 
 
-// /edit/:id : Editable of Selected Comic (GET)
+// /edit/:id : Editable of Selected Comic (GET) TODO:
+
+app.get('/edit/:id', editComic);
+
+function editComic(request, response){
+  // response.send('howdy!')
+  let id = request.params.id;
+  let sql = 'SELECT * FROM comics WHERE id=$1;';
+  let safeValues = [id];
+
+  client.query(sql, safeValues)
+    .then(results => {
+      console.log(results);
+      let bookToEdit = results.rows[0]; // as of 07-28 only one object within rows will be returned, hence is known w finality that the only response will be rows 0 of that array.
+      response.status(200).render('pages/edit.ejs', {incomingComic:bookToEdit});
+    }).catch((error) => {
+      console.log('ERROR', error);
+      response.status(500).send('MEOW!!! %*.*%');
+    })
+}
 
 
 
